658590484f483f8dccba0837416e1011ac721d4e
Chris Wand <chris.wand@gmail.com>
2014-12-15 13:30:18 -0500

    Initializing branch

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,13 @@
 class QuestionsController < ApplicationController
-
   def index
     @questions = Question.all
   end
 
+  def new
+
+  end
+
+  def create
+    question = Question.new(params)
+  end
 end
